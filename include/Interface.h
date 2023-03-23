8f34cadcd84ceac5a97f5f25ff3d37fb1f81794c
Resistor10k1 <55755273+Resistor10k1@users.noreply.github.com>
2023-03-23 19:08:18 +0100

    Implement Interface.h

---
+++ include/Interface.h
@@ -0,0 +1,47 @@
+/**
+ * @file    Interface.h
+ * @brief   Example interface
+ * @date    2023-03-23
+ * @author  Andri Trottmann
+ */
+
+#pragma once
+
+#include <iostream>
+
+
+/**
+ * @brief Abstract class
+*/
+class Interface
+{
+    public:
+
+        /**
+         * @brief Interface Constructor
+        */
+        Interface() {}
+        
+        /**
+         * @brief Interface Deconstructor
+        */
+        virtual ~Interface() {};
+
+        /**
+         * @brief Pure virtual function
+         * @return Returns 0 if successful.
+        */
+        virtual int doSomething(void) = 0;
+
+        /**
+         * @brief Returns the ID
+         * @return The ID of datatype int.
+        */
+        int getId(void) const
+        {
+            return id;
+        }
+
+    protected:
+        int id = 111; /**< Identifier */
+};
