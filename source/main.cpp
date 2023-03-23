8a7083bf4b7f186fd426a0c130b045686327455d
Resistor10k1 <55755273+Resistor10k1@users.noreply.github.com>
2023-03-23 17:51:30 +0100

    Create main.cpp

---
+++ source/main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file    main.cpp
+ * @brief   Main application entry point.
+ * @date    2023-03-23
+ * @author  Andri Trottmann
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(void)
+{
+    cout << "This is the application main" << endl;
+    cout << "Additional output ..." << endl;
+    return 0;
+}
